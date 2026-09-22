5b29db6bfc6f1d2e2c7b2fd2f6ec1a9bfa368c83
Travis Jacobs <byfanzoftheone@gmail.com>
2026-09-22 08:59:07 -0600

    fix: orbit chase cam 45deg left of rear + bust SW cache

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,28 @@
-const CACHE = 'subi-life-offline-v2';
+const CACHE = 'subi-life-offline-v3';
 const CORE = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)));
 });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
 self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate' || (e.request.headers.get('accept') || '').includes('text/html')) {
+    e.respondWith(fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match('/index.html')));
+    return;
+  }
   e.respondWith(
     caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
       const copy = res.clone();
       caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
       return res;
-    }).catch(() => caches.match('/index.html')))
+    }))
   );
 });
